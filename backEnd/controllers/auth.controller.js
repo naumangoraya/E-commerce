43531d8be0334f299c82ee681cc279e9848ed137
naumangoraya <naumanjaat@gmail.com>
2025-04-06 12:54:30 +0500

    intial

---
+++ backEnd/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("sign up route called");
+};
+
+export const signin = async (req, res) => {
+  res.send("sign in route called");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout route called");
+};
